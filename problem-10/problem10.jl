f469b114fdec50060594b57baf76794d4046d0e9
Juntao Ren <kevinrjt@163.com>
2020-06-27 13:34:26 +0800

    Solve problem 10

---
+++ problem-10/problem10.jl
@@ -0,0 +1,42 @@
+# https://projecteuler.net/problem=10
+
+# Solution 1
+function primes_below(n)
+    primes = []
+    is_prime = trues(n)
+    limit = floor(Int, sqrt(n-1))
+    for i in 2:n-1
+        if is_prime[i]
+            append!(primes, i)
+            if i > limit continue end
+            for j in i*i:i:n-1
+                is_prime[j] = false
+            end
+        end
+    end
+    return primes
+end
+
+primes = primes_below(2000000)
+println(sum(primes))
+
+# Solution 2: saves half memory
+function primes_below(n)
+    primes = [2]
+    m = (n - 2) ÷ 2
+    is_prime = trues(m)
+    limit = (floor(Int, sqrt(n-1)) - 1) ÷ 2
+    for k in 1:m
+        if is_prime[k]
+            append!(primes, 2k + 1)
+            if k > limit continue end
+            for j in 2k*(k+1):2k+1:m
+                is_prime[j] = false
+            end
+        end
+    end
+    return primes
+end
+
+primes = primes_below(2000000)
+println(sum(primes))
